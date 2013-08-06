86048cfd6ed27e761d7913467d5d98e5905a57a1
N.Ardi <ardi@detik.com>
2013-08-07 06:57:40 +0700

    update comment.

--- jquery.ngajaxUpload.js
+++ jquery.ngajaxUpload.js
@@ -1,8 +1,34 @@
+/*
+ * jquery.ngajaxUpload.js
+ * 
+ * Copyright 2013 Ardi <noone.nu1@gmail.com>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
 ;(function($){
     $.fn.ngajaxUpload = function (opts) {
-        var options = $.extend(true, {
-                after: function(data) {},
+        var options = $.extend(true,
+            {
+                after: function(data) {}, // in after scope, "this" is iframe.
                 before: function(event) {
+                    // return true, so form submitted.
+                    // in this scope (before), "this" is the form.
                     return true;
                 }
             }
