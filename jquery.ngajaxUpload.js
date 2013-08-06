50890c92265aca6bec57f31ee082c23762f74d10
N.Ardi <ardi@detik.com>
2013-08-07 06:49:39 +0700

    Update option, use before and after options name.

--- jquery.ngajaxUpload.js
+++ jquery.ngajaxUpload.js
@@ -1,10 +1,12 @@
 ;(function($){
     $.fn.ngajaxUpload = function (opts) {
         var options = $.extend(true, {
-                callback: function(data) {
-                    console.log(data);
+                after: function(data) {},
+                before: function(event) {
+                    return true;
                 }
-            }, opts);
+            }
+            , opts);
 
         return this.each(function(index, obj) {
             var iframe = document.createElement('iframe');
@@ -13,11 +15,15 @@
             document.body.appendChild(iframe);
 
             obj.target = 'ngajax-upload-iframe';
+            $(obj).submit(function(event){
+                return options['before'].call(this, event);
+            })
+            
             $('#ngajax-upload-iframe').on('load', function(a,b,c){
                 var res = $(this).contents().text();
 
-                if (typeof(options['callback']) == 'function') {
-                    options['callback'].call(this, res);
+                if (typeof(options['after']) == 'function') {
+                    options['after'].call(this, res);
                 }
             })
         })
